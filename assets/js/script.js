cf1e283eb2d354e2ef3598e8c6cc4d42e48ed026
SteelingLight <alecholgate@earthlink.net>
2021-11-01 03:50:29 -0600

    story work

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,6 @@ var boatNodes = [
                         text: "",
                         nextNode: 1
                     },
-                   
                 ]
             },
             {
