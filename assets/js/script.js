5340c40231b6a5069c4d59d32cf99de46db5111d
SteelingLight <alecholgate@earthlink.net>
2021-11-01 08:55:55 -0600

    story work

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ var mainPageScreen = document.getElementById("main-page");
 var selectCharOne = document.getElementById("charOne-button");
 var selectCharTwo = document.getElementById("charTwo-button");
 
+var quess = "";
+var ans = "";
 
 document.getElementById("log-wrapper").addEventListener("click", function () {
     textInput.focus();
@@ -40,25 +42,23 @@ var boatNodes = [
             },
             {
                 id: 1,
-                text: "As you step through these picturesque environs, your skin warmed and invigorated by the welcomed explosion of nature's forces, a gentle calm descends upon you. A peace more deep than any you have ever felt caresses your conscience as the bright fileds of warmth spread before you. Gradually, a weight builds behind your eyes and you find your limbs growing slack. A determination calls to you, pulling you downwards. As if into the plains themself. Aroused by the curiousity of your predicament you push yourself deeper into this beautiful nightmare. The calm gait with which you had started, shambles into a slurred sleepy wobble, and shattering into a frenzied lurch before you finally collapse on hands and knees spread out beneath the staring sky in supplication. A shadow falls over you. FIGHT",
+                music: "Ludwig Van Beethoven",
+                text: "As you step through these picturesque environs, your skin warmed and invigorated by the welcomed explosion of nature's forces, a gentle calm descends upon you. A peace more deep than any you have ever felt caresses your conscience as the bright fileds of warmth spread before you. Gradually, a weight builds behind your eyes and you find your limbs growing slack. A determination calls to you, pulling you downwards. As if into the plains themself. Aroused by the curiousity of your predicament you push yourself deeper into this beautiful nightmare. The calm gait with which you had started, shambles into a slurred sleepy wobble, and shattering into a frenzied lurch before you finally collapse on hands and knees spread out beneath the staring sky in supplication. A shadow falls over you. From within a primitive sensations burns, commanding you to GAZE upon the entity that has come before you.",
                 options: [
                     {
-                        text: "fight",
+                        text: "gaze",
                         nextNode: 2
                     }
                 ]
             },
             {
                 id: 2,
-                text: "You stand up from your bed to the floor of the ships cabin. You don't remember how you even got on a ship in the first place. LEAVE the room or peer out your WINDOW.",
+                music: "Johann Sebastian Bach",
+                text: "From within the shadow that now looms over you a burning presence sears against not only your flesh, but the very threshold of your mind quakes. Layers of consciousness strip and peel away beneath its enshackling contemplation. As though a child caught before an irritable authority you mewl and shrivel inwards, caving unto yourself as you AWAIT your penance.",
                 options: [
                     {
-                        text: "window",
+                        text: "wait",
                         nextNode: 3,
-                    },
-                    {
-                        text: "leave",
-                        nextNode: 5
                     }
                 ]
             },
@@ -66,75 +66,173 @@ var boatNodes = [
 
 
                 id: 3,
-                text: "You awake. Icey cold shocks your sleep adled mind leaving you in momentary paralysis as the memories of what you saw receed and fade. Only a slight hint of fury and malignance of the entity remains. That is enough. The world remains a dark prison of emptiness defined by the water that sloshed at your feet and the taunting cold that determinedly dogged at your body's nerves. WAIT",
+                music: "Johann Sebastian Bach",
+                text: "Nothing happens. Darkness, a comforting oblivion. Peace. For a moment you lie limbs adrift and thoughts a gently flowing trickle of contemplation beholding nothing but quiet serentity. Even this does not last. Eventually you visciously arrive in some semblance of reality, jostled upright and sheathed in an grimy musk of sweat from a harried and strenuous sleep. At the bed side a smattering of personal accoutrements most notably your ADARI blade, the GLINTEL locket, and a parchment of MEDICINAL herb. Without you tailored coat it's unlikely you'll be able to carry all of them. ",
                 options:[
                     {
-                        text: "wait",
+                        text: "adari",
+                        setState:{adari: true, note1: true, logItem: "You have picked up your Adari,", text:"Adari"},
                         nextNode: 4,
+                    },
+                    {
+                        text: "medicinal",
+                        setState:{medicine: true, note1: true, logItem: "You have picked up the parchemnt,", text:"Medicine"},
+                        nextNode: 5,
+                    },
+                    {
+                        text: "glintel",
+                        setState:{locket: true, note1: true, logItem: "You have picked up your locket,", text:"Locket"},
+                        nextNode: 6
                     }
                 ]
             },
             {
                 id: 4,
-                text: "Gradually as your eyes adjust to the little light available an entire room opens up around you. Defined by the sharp contours of its walls and what meaningless furniture and equipment was stowed here. Inset into one wall a simple DOOR stands ajar, a slight crack gives entry to a stronger though muted source of light beyond. Directly opposite your own position another access-way, a small HATCH opens to a downward falling ladder. Of all the rooms noteable furniture an overturned DESK appears to be lying haphazardly, angled as though something beneath it had roughly intercepted its descent.",
+                music: "Johann Sebastian Bach",
+                text: "Reverently you sheath the blade at yourside. Still the GLINTEL locket and the MEDICINAL pouch remain.",
                 options:[
                     {
-                        text: "door",
+                        text: "glintel",
+                        setState:{locket: true, note1: true, logItem: "You have picked up your locket,", text:"Locket"},
                         nextNode: 7,
                     },
                     {
-                        text: "hatch",
-                        nextNode: 6,
-                    },
-                    {
-                        text: "desk",
-                        nextNode: 5,
+                        text: "medicinal",
+                        setState:{medicine: true, note1: true, logItem: "You have picked up the parchemnt,", text:"Medicine"},
+                        nextNode: 7,
                     }
                 ]
             },
             {
                 id: 5,
-                text: "You approach the desk. Gingerly you shy around the bulky objects tortured angle to the face on which it lies turned. Beneath it a small crate has caved, exposing a handful of coins. Rummaging about the desks various secrets and crannies you discover a small nondescript knife and some personal note. LEAVE",
+                music: "Johann Sebastian Bach",
+                text: "Carefully you tuck the parchemnt of herb against the hem of your pants before considering the remaining items. Your personal ADARI gleams in the warm burning light of the evening. Similarly the GLINTEL locket dances with rippling affectation as the rooms bobs and sways.",
                 options: [
                     {
-                        text: "leave",
-                        setState: {knife: true, note1: true, logItem: "You have picked up a knife, 15 credits, and a note", text:"Knife", altercredits: 15},
-                        nextNode: 4
+                        text: "adari",
+                        setState:{adari: true, note1: true, logItem: "You have picked up your Adari,", text:"Adari"},
+                        nextNode: 7
                     },
                     {
-                        text: "",
-                        setState: {knife: true, note1: true, logItem: "You have picked up a knife, 15 credits, and a note", text:"Knife", altercredits: 15},
-                        nextNode: 4
+                        text: "glintel",
+                        setState:{locket: true, note1: true, logItem: "You have picked up your locket,", text:"Locket"},
+                        nextNode: 7
                     }
 
                 ]
             },
             {
                 id: 6,
-                text: "You approach the hatch. A dark tunnel travels downward. At the bottom a small blue light flicks at irregular intervals. You recall that to DESCEND would lead you further into the ship. Behind you the ROOM remains tranquil.",
+                music: "Johann Sebastian Bach",
+                text: "In a hurried rustle of metalic links the locket falls weightlessly around your neck. The familiar cold of the metal presses gently against your chest as you consider the sharp ADARI and the soothing MEDICINAL herbs arrayed before you.",
                 options: [
                     {
-                        text: "descend",
-                        nextNode: 8,
+                        text: "adari",
+                        setState:{adari: true, note1: true, logItem: "You have picked up your Adari,", text:"Adari"},
+                        nextNode: 7,
                     },
                     {
-                        text: "room",
-                        nextNode: 4,
+                        text: "medicinal",
+                        setState:{medicine: true, note1: true, logItem: "You have picked up the parchemnt,", text:"Medicine"},
+                        nextNode: 7,
                     }
                 ]
             },
             {
                 id: 7,
-                text: "You approach the door. ",
+                music: "Wolfgang Amadeus Mozart",
+                text: "With your items collected a cursory glance around the room informs you that you have awoken in your chambers. At the WINDOW a orange sky rages in the fading evening. The unsteady FEELING of the rooms warbling and softly shifting motions recollects the information of having traveled on some vessel. Most notably the two exits of the access HATCH and the DOOR seem to have been jostled by some hitherto unknown presence. ",
                 options: [
                     {
-                        text: "exit",
+                        text: "window",
                         nextNode: 8,
                     },
                     {
-                        text: "room",
-                        nextNode: 4,
+                        text: "feeling",
+                        nextNode: 9,
                     },
+                    {
+                        text: "door",
+                        nextNode: 10,
+                    },
+                    {
+                        text: "hatch",
+                        nextNode: 11,
+                    },
+                ]
+            },
+            {
+                id: 8,
+                music: "Wolfgang Amadeus Mozart",
+                text: "Outside the sky is ablaze in fiery yellows and near crimson reds as the underside of the few prominent clouds devour the warmth of the sun in their sea of near-dark indigo. A moment your breath catches and you are back in your dream, bare before some cthonic entity vaster than even the impenetrable heavens. Looking down at the gentle swell the water glimmmers and shines as small waves lap playfully into the suns embrace. The ROOM waits behind you.",
+                options: [
+                    {
+                        text: "room",
+                        nextNode: 7,
+                    }
+                ]
+            },
+            {
+                id: 9,
+                music: "Wolfgang Amadeus Mozart",
+                text: "Though the near rhythmic thrum of the tide against the hull and its casual movements remains nearly intact a definitive distinction is present in the groaning shudder that seems to resonate through the vessels frame. It is possible that your journey, and arrival, have not been wholly untoward. Around you the ROOM remains an infallible calm.",
+                options: [
+                    {
+                        text: "room",
+                        nextNode: 7,
+                    }
+                ]
+            },
+            {
+                id: 10,
+                music: "Wolfgang Amadeus Mozart",
+                text: "The small room takes only a moment to cross. After a few unhurried strides you stare out the doorsways threshold. A small cargo hallway eerily dim and unkempt reaches out - clawing. You step back a moment, your breath quickening before your training reasserts itself. The comforting presence of the ROOM remains, but the path forward is best TREAD carefully.",
+                options: [
+                    {
+                        text: "room",
+                        nextNode: 7,
+                    },
+                    {
+                        text: "tread",
+                        nextNode: 12,
+                    }
+                ]
+            },
+            {
+                id: 11,
+                music: "Wolfgang Amadeus Mozart",
+                text: "Idling around the length of your bed you stare down at the small access hatch. Normally reserved for elite passengers and those few well acquainted with the vessel the tiny passage way has been left unlocked. Though the ROOM remains in relative calm, the PATH before you is of interest.",
+                options: [
+                    {
+                        text: "room",
+                        nextNode: 7,
+                    },
+                    {
+                        text: "path",
+                        nextNode: 13,
+                    }
+                ]
+            },
+            {
+                id: 12,
+                music: "Igor Stravinsky",
+                text: "A sworn member of the Voltanese Sword Arm you defy the swelling unease that builds within you as you first sojourn beyond the confines of your amiable room. A few halting steps into the darkness and you pause, a quick glance back. As you do so the Door slams with an uncanny crazck. The hallway and BEYOND is your only solace now.",
+                options: [
+                    {
+                        text: "beyond",
+                        nextNode: 14,
+                    }
+                ]
+            },
+            {
+                id: 14,
+                music: "Igor Stravinsky",
+                text: "",
+                options: [
+                    {
+                        text: "beyond",
+                        nextNode: 14,
+                    }
                 ]
             }
 
@@ -404,10 +502,7 @@ function startChapter() {
 items = {};
 storyLog(0);
 musicLog("Ludwig Van Beethoven");
-
-
 };
-
 
 
 document.getElementById("reset-button").addEventListener("click", function(){
@@ -419,8 +514,6 @@ document.getElementById("save-button").addEventListener("click", function(){
     saveGame();
 });
 
-startChapter();
-
 
 function saveGame() {
 var player = JSON.stringify(playerObj);
@@ -430,3 +523,4 @@ var playerGrab = localStorage.getItem("Player Stats");
 JSON.parse(playerGrab);
 console.log(playerGrab);
 };
+
